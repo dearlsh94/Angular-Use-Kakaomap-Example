134b1ff1b7a6c8df9ee32d011db12c3eeb508247
dearlsh94 <dearlsh94@gmail.com>
2020-12-03 16:57:31 +0900

    change init point

--- src/app/kakaomap/kakaomap.component.ts
+++ src/app/kakaomap/kakaomap.component.ts
@@ -17,8 +17,7 @@ export class KakaomapComponent implements OnInit {
     */
     var container = document.getElementById('map');
     var options = {
-      // center: new kakao.maps.LatLng(33.450701, 126.570667),
-      center: new kakao.maps.LatLng(37.483005820136626, 127.00398550608922),
+      center: new kakao.maps.LatLng(33.450701, 126.570667),
       level: 3
     };
     var map = new kakao.maps.Map(container, options);
